9ecdc9663b9e438a970184b54d0b6b9d93cebe6c
alexsavkaev <lesh1y174@gmail.com>
2023-03-09 21:25:41 +0300

    added prompt method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System;
+using static System.Console;
+Clear();
+string Prompt(string message)                                  // Функция для ввода переменных
+{
+    Write(message);                                            // выводим заданный текст на экран
+    string result = ReadLine();                                // считываем введённые данные в строку                     
+    return result;
+}
