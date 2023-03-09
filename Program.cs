24df5579d58e04c9f46f64f2b0752e766dc15ab8
alexsavkaev <lesh1y174@gmail.com>
2023-03-09 22:10:53 +0300

    added comments

--- Program.cs
+++ Program.cs
@@ -12,28 +12,28 @@ string[] StringToArray(string stringArray)                     // Функция
     string[] result = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries); 
     return result;
 }
-string[] IfFewer(string[] inArray)
+string[] IfFewer(string[] inArray)                              // Функция для выбора слов, состоящих из <= 3 букв
 {
-    int count = 0;
+    int count = 0;                                              // Счётчик для нахождения длины финального массива и его наполнения
     for (int i = 0; i < inArray.Length; i++)
     {
         string element = inArray[i];
         if (element.Length <= 3) count += 1;
     }
-    string[] result = new string[count];
-    count = 0;
+    string[] result = new string[count];                        // Создаём массив длиной равной счётчику
+    count = 0;                                                  // Обнуляем счётчик для наполнения массива
     for (int i = 0; i < inArray.Length; i++)
     {
         string element = inArray[i];
         if (element.Length <= 3)
         {
-            result[count] = element;
-            count++;
+            result[count] = element;                            // Каждое подходящее слово добавляем в массив
+            count++;                                            // Увеличиваем счётчик
         }
     }
-    return result;
+    return result;                                              // Возвращаем получившийся массив
 }
-string textline = Prompt("Введите слова через пробел: ");
-string[] text = StringToArray(textline);
-Write("Слова, в которых букв меньше или равно 3: ");
+string textline = Prompt("Введите слова через пробел: ");       // Указания для пользователя и создание строки с введёнными словами
+string[] text = StringToArray(textline);                        // Преобразуем строку в массив с отдельными словами
+Write("Слова, в которых букв меньше или равно 3: ");            // Выводим результат
 WriteLine(string.Join(", ", IfFewer(text)));
